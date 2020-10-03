f0f7392cf6f8bdfb3755716b8288ec0fa0537da0
cha-han851 <tomo45458@gmail.com>
2020-10-03 12:53:30 +0000

    update setting.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+
+
 import MySQLdb
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i-2**r@k8!r**x$mo&z$k%zm2n+_6k!1nktg3$8aqhlsjj@5wv'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
